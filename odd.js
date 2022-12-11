50062fb5c6bb3b7d7d91f1803ded02378ee2741e
Denny Doan <denny-333@hotmail.com>
2022-12-10 23:23:27 -0500

    Added odd checker

---
+++ odd.js
@@ -0,0 +1,7 @@
+// define function isOdd
+// function should take a number as a parameter and return true if it is odd and false if otherwise
+var isOdd = function(num){
+    return num % 2 !==0;
+}
+console.log(isOdd(11))
+console.log(isOdd(10))
